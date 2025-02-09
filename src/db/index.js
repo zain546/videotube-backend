2dec872569963f92878f76b7d537a2b9cd259619
Asad Ali Developer <uhope1645@gmail.com>
2025-02-09 17:45:07 +0500

    Added concurrently and write new script

---
+++ src/db/index.js
@@ -0,0 +1,21 @@
+import mongoose from "mongoose";
+import { DB_NAME } from "../constants.js";
+import dotenv from "dotenv";
+
+dotenv.config();
+
+const connectDB = async () => {
+  try {
+    const connectionInstance = await mongoose.connect(
+      `${process.env.MONGODB_URI}/${DB_NAME}`
+    );
+    console.log(
+      `MongdoDB connected!! DB HOST: ${connectionInstance.connection.host}`
+    );
+  } catch (error) {
+    console.log("MongoDB connection Failed", error);
+    process.exit(1);
+  }
+};
+
+export default connectDB;
