0b3c7be29bb2b532d79509ec820d4b5fc21a0383
Asad Ali Developer <uhope1645@gmail.com>
2025-02-09 17:49:03 +0500

    Did

--- src/index.js
+++ src/index.js
@@ -2,6 +2,7 @@ import dotenv from "dotenv";
 import connectDB from "./db/index.js";
 import { app } from "./app.js";
 
+// Configured Dotenv
 dotenv.config({ path: "./.env" });
 
 // Done by Asad-Ali-Developer!
