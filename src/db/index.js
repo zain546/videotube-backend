18133e4fe1ba81f5101674e156b42aee0ff61224
Zain Ali <zainali187483@gmail.com>
2025-02-10 09:14:24 +0500

    Resolve mongoDB connection issue

--- src/db/index.js
+++ src/db/index.js
@@ -1,5 +1,7 @@
 import mongoose from "mongoose";
 import { DB_NAME } from "../constants.js";
+import dotenv from "dotenv";
+dotenv.config()
 
 const connectDB = async ()=>{
     try{
--- src/index.js
+++ src/index.js
@@ -1,10 +1,12 @@
 import express from "express";
 import dotenv from "dotenv";
-import connectDB from "./db/connection.js";
+import connectDB from "./db/index.js"
 const app = express()
 
 dotenv.config({path: "./.env"});
 
-connectDB();
-
-app.listen(process.env.PORT,()=>{`app is listening on port ${process.env.PORT}`})
+connectDB().then(()=>{
+    app.listen(process.env.PORT,()=>{`app is listening on port ${process.env.PORT}`})
+}).catch((error)=>{
+    console.log(error)
+})
