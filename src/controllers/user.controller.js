c8f46db380c8c3f74b8b76a3753038d7933b5a8f
Zain Ali <zainali187483@gmail.com>
2025-02-21 12:16:45 +0500

    Add user channel profile retrieval with subscriber counts and subscription status

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -291,6 +291,74 @@ const updateUserCoverImage = asyncHandler(async (req, res) => {
     .json(new ApiResponse(200, user, "CoverImage updated successfully"));
 });
 
+const getUserChannelProfile = asyncHandler(async (req, res) => {
+  const { username } = req.params;
+  if (!username) {
+    throw new ApiError(400, "username is missing");
+  }
+  const channel = await User.aggregate([
+    {
+      $match: {
+        username: username?.toLowerCase(),
+      },
+    },
+    {
+      $lookup: {
+        from: "subscriptions",
+        localField: "_id",
+        foreignField: "channel",
+        as: "subscribers",
+      },
+    },
+    {
+      $lookup: {
+        from: "subscriptions",
+        localField: "_id",
+        foreignField: "subscriber",
+        as: "subscribedTo",
+      },
+    },
+    {
+      $addFields: {
+        subscribersCount: {
+          $size: "$subscribers",
+        },
+        channelSubscribedToCount: {
+          $size: "$subscribedTo",
+        },
+        isSubcribed: {
+          $cond: {
+            if: { $in: [req.users?._id, "$subcribers.subcriber"] },
+            then: true,
+            else: false,
+          },
+        },
+      },
+    },
+    {
+      $project:{
+        fullName:1,
+        username:1,
+        email:1,
+        subscribersCount:1,
+        channelSubscribedToCount:1,
+        isSubcribed:1,
+        avatar:1,
+        coverImage:1,
+        createdAt:1,
+      }
+    }
+  ]);
+  if(!channel?.length){
+    throw new ApiError(404,"channel does not exists.")
+  }
+  return res
+  .status(200)
+  .json(
+    new ApiResponse(200, channel[0], "User channel profile fetched successfully")
+  )
+});
+
 export {
   registerUser,
   loginUser,
@@ -301,4 +369,5 @@ export {
   getCurrentUser,
   updateUserAvatar,
   updateUserCoverImage,
+  getUserChannelProfile
 };
--- src/models/subcription.model.js
+++ src/models/subcription.model.js
@@ -11,4 +11,4 @@ const subscriptionSchema = new Schema({
     }
 }, { timestamps: true });
 
-export const Subscription = mongoose.model("Subscription", subcriptionSchema);
+export const Subscription = mongoose.model("Subscription", subscriptionSchema);
